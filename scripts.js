e1573a50d5ce377ccd01539325b6ed31d0a15162
APWaldo <apw2131@columbia.edu>
2014-02-18 12:42:06 -0500

    Added new json functions to scripts.

--- scripts.js
+++ scripts.js
@@ -4,17 +4,37 @@
 
 console.log("javascript file loaded");
 
+function loadedJSON(RISHI){
+	//console.log the "count" property of our json.
+	//I expect for the output to be 409.
+	console.log (RISHI.count);
+}
+
 /*
  * This is my custom function called dataLoaded.
  */
 function dataLoaded(){
+	
 	console.log("I got to document ready!");
+
+	 //this is jquery shorthand to create a new div element.
+	var myDiv = $("<div>");
+	//use jquery to add some text to it.
+	//hey jquery, take that dive i just created called "myDiv" and add some html (in this case it's just a string.)
+	$(myDiv).html("This is my new div");
 	
-	
-	
-	
-	
-	
+	//hey jquery, grab the div with the id contentContainer and put my new div inside it. Do this with the function append.
+	//#--this means look for the element with this "id" attribute. the "." means look for the elment(s) that has the "class" 
+	//attribute, ids should always be unique, but classes can be used for multiple. I expxect to see th enew div containing
+	//the text "This is my new div" appear inside the div with the id "contentContainer".
+	$("#contentContainer").append(myDiv);
+
+	//use the jquery to load my json file, it takes three parameters. 
+	//first is the name of my file.
+	//second parameter is the function to call once the file is loaded.
+	//third parameter is a string of the file type to expect. 
+	$.get("UEMP270V_data.json", loadedJSON, "json");
+
 	
 }
 /*
